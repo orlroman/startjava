d8a240b9d7ce273315fce38b69404bb682365f49
Roman Orlov <main.r.orlov@gmail.com>
2024-03-10 19:20:41 +0300

    Added sql files

---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,57 @@
+\echo ВСЕ роботы
+SELECT * 
+    FROM jaegers
+    ORDER BY model_name;
+
+\echo Не уничтоженные работы
+SELECT * 
+    FROM jaegers
+    WHERE status = 'Active'
+    ORDER BY model_name;
+
+\echo Роботы Mark-1, Mark-4
+SELECT * 
+    FROM jaegers
+    WHERE mark IN (1, 4)
+    ORDER BY model_name;
+
+\echo Все роботы, кроме mark-1 и mark-4
+SELECT * 
+    FROM jaegers
+    WHERE mark NOT IN (1, 4)
+    ORDER BY model_name;
+
+\echo Самый старый робот 
+SELECT * 
+    FROM jaegers
+    WHERE launch <= (SELECT MIN(launch)
+                        FROM jaegers)
+    ORDER BY model_name;
+
+\echo Роботы, которые уничтожили больше всех kaiju
+SELECT model_name, mark, launch, kaiju_kill
+    FROM jaegers
+    WHERE kaiju_kill >= (SELECT MAX(kaiju_kill)
+                            FROM jaegers)
+    ORDER BY model_name;
+
+\echo Средний вес роботов
+SELECT ROUND(AVG(weight), 3) AS avg_weight
+    FROM jaegers;
+
+\echo Увеличение количества убитых kaiju на 1 у не уничтоженных роботов
+UPDATE jaegers
+    SET kaiju_kill = kaiju_kill + 1;
+    WHERE status = 'Active';
+
+SELECT *
+    FROM jaegers
+    ORDER BY model_name;
+
+\echo Удаление уничтоженных роботов
+DELETE FROM jaegers
+    WHERE status = 'Destroyed';
+
+SELECT *
+    FROM jaegers
+    ORDER BY model_name;
